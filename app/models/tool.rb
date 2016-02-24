fd509fbbd005a45fc0d1e15c8b062ae93d1799f4
David Whitaker <damwhit@gmail.com>
2016-02-24 09:11:50 -0700

    added drop down button for categories

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,6 +1,7 @@
 class Tool < ActiveRecord::Base
   validates :name, presence: true
   belongs_to :user
+  belongs_to :category  
   # this prevents you from submitting a form with a missing name attribute
   def formatted_price
     price / 100.0
